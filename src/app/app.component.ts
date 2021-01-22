7e86e7e2b9e2f31052b0c9a5b2e05d85ec80cf1e
Ryan Rambali <ryanrambali@gmail.com>
2021-01-21 22:47:38 -0500

    Implemented Dynamic Ttitles With a Data Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NbSidebarService } from '@nebular/theme';
+import { DataService } from "./data.service";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
+ 
   title = 'Investment-Taxation';
 
-  constructor(private sidebarService: NbSidebarService) {
+  message!: string;
+  subscription!: Subscription;
+
+  constructor(private data: DataService, private sidebarService: NbSidebarService) {}
+
+  ngOnInit() {
+    this.subscription = this.data.currentMessage.subscribe(message => this.message = message)
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   toggle() {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DataService } from "../data.service";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  message!:string;
+  subscription!: Subscription;
 
-  ngOnInit(): void {
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.subscription = this.data.currentMessage.subscribe(message => this.message = message)
+    this.data.changeMessage("Home")
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
